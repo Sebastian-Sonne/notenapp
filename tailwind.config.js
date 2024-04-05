71ae37ddc751f05f68f437c3332051c6e262f955
sebastian-sonne <sebastian@familie-sonne.com>
2024-04-05 13:08:14 -0500

    small screen optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}", "./assets/js/*.js"],
@@ -7,6 +9,10 @@ module.exports = {
         'notenapp-blue' : '#008ee6',
         'notenapp-blue-hover' : '#006eb2',
       },
+    },
+    screens: {
+      'xs': '400px',
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
