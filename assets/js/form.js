b348d47fe3c4f1bcb4480e0ef104b6c3f9f777ec
sebastian-sonne <sebastian@familie-sonne.com>
2024-04-06 22:12:38 -0500

    error detection update

--- assets/js/form.js
+++ assets/js/form.js
@@ -152,15 +152,37 @@ export function validateInput(input, leaving = false) {
 export function getFormErrorMessage(inputId, value) {
     switch (inputId) {
         case 'id':
-            return value.length < 6 ? 'ID muss mindestens 6 Zeichen lang sein' : (value.length > 16 ? 'ID darf maximal 16 Zeichen lang sein' : '');
+            if (!/^\d+$/.test(value)) {
+                return 'ID darf nur aus Zahlen bestehen';
+            }
+            if (value.length < 6) {
+                return 'ID muss mindestens 6 Zeichen lang sein';
+            }
+            if (value.length > 16) {
+                return 'ID darf maximal 16 Zeichen lang sein';
+            }
+            return '';
         case 'name':
-            return value.length < 1 ? 'Name ist erforderlich' : (value.length > 40 ? 'Name überschreitet maximale Länge' : '');
+            if (value.length < 1) {
+                return 'Name ist erforderlich';
+            }
+            if (value.length > 40) {
+                return 'Name überschreitet maximale Länge';
+            }
+            return '';
         case 'email':
-            return value.length < 1 ? 'E-Mail ist erforderlich' : (validateEmailPattern(value) ? '' : 'Ungültige E-Mail');
+            if (value.length < 1) {
+                return 'E-Mail ist erforderlich';
+            }
+            if (!validateEmailPattern(value)) {
+                return 'Ungültige E-Mail';
+            }
+            return '';
         default:
             return '';
     }
 }
+
 
 /**
  * function to validate an email
